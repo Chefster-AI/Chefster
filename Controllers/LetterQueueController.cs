4f48b32c1c88a2f13fc6f4e18ff6ae1230359629
Parker W Christy <pchristy03@gmail.com>
2024-09-07 08:23:48 -0500

    use apiKey for letter queue request

--- Controllers/LetterQueueController.cs
+++ Controllers/LetterQueueController.cs
@@ -1,25 +1,38 @@
 using Chefster.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Chefster.Controllers;
 
-#if DEBUG
 [Authorize]
 [Route("api/letter-queue")]
 [ApiController]
-public class LetterQueueController(LetterQueueService letterQueueService) : ControllerBase
+public class LetterQueueController(
+    LetterQueueService letterQueueService,
+    IConfiguration configuration
+) : ControllerBase
 {
     private readonly LetterQueueService _letterQueueService = letterQueueService;
+    private readonly IConfiguration _configuration = configuration;
 
     /// <summary>
     /// Populates a google sheet with customer information for sending personal letters
     /// </summary>
     [HttpPost]
-    public void PopulateLetterQueue()
+    public ActionResult PopulateLetterQueue([FromBody] string apiKey)
     {
-        _letterQueueService.PopulateLetterQueue();
+        if (apiKey.IsNullOrEmpty())
+        {
+            return BadRequest("Unauthorized!");
+        }
+
+        if (apiKey == _configuration["LETTER_QUEUE_KEY"])
+        {
+            _letterQueueService.PopulateLetterQueue();
+            return Ok();
+        }
+
+        return BadRequest("Unauthorized!");
     }
 }
-
-#endif
